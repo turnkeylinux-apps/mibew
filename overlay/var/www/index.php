8bca4562bed52848a13aae9140aaf8f53006c674
Anton Pyrogovskyi <anton@turnkeylinux.org>
2015-08-13 19:55:28 +0000

    Fix paths for chat button as per current Mibew

--- overlay/var/www/index.php
+++ overlay/var/www/index.php
@@ -74,7 +74,7 @@
         </div>
 
         <div id="mibew-chat">
-	    <a href="/client.php?locale=en" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 &amp;&amp; window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('/client.php?locale=en&amp;url='+escape(document.location.href)+'&amp;referrer='+escape(document.referrer), 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="/b.php?i=mgreen&amp;lang=en" border="0" width="163" height="61" alt=""/></a>
+	    <a href="/chat?locale=en" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 &amp;&amp; window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open('/chat?locale=en&amp;url='+escape(document.location.href)+'&amp;referrer='+escape(document.referrer), 'webim', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="/b?i=mgreen&amp;lang=en" border="0" width="163" height="61" alt=""/></a>
         </div>
 
     </body>
